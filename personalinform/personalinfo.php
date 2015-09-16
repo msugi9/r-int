934fbed661679a0411f1dd66a002ca5f5c86112f
nikotaman05 <nikotaman05@ze.gs>
2015-09-16 17:59:41 +0900

    add

--- personalinform/personalinfo.php
+++ personalinform/personalinfo.php
@@ -1,5 +1,36 @@
 <?php
-echo $_REQUEST['login_id'];
-echo $_REQUEST['login_pass'];
+$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
+$url = parse_url($database_url);
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+try{
+    //データベース接続
+    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+    $loginid=$_REQUEST[login_ID];
+    $pass=$_REQUEST[login_pass];
+    $height=$_REQUEST[height];
+    $weight=$_REQUEST[weight];
+    $shoe=$_REQUEST[shoe_size];
+    $accessory=$_REQUEST[accessory];
+    $wear=$_REQUESRT[wear];
+    $board=$_REQUEST[board];
+    $email=$_REQUEST[email];
+    
+    $insert_sql = "insert into personal ($loginid,'$pass',$height,$weight,$shoe,$accessory,$wear,$board,'$email')";
+
+    $sql = $pdo->exec($insert_sql); 
+
+    echo "登録完了しました。"
+    
+    echo "身長...'$height'\n"
+    echo "体重...'$weight'\n"
+    echo "靴のサイズ...'$shoe'\n"
+    //echo "アクセサリー(キャップ、グローブ、ゴーグル)...'$accessory'\n"
+    //echo "ウェア...'$wear'\n"
+    //echo "ボード...'$board'\n"
+    
+}catch(PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+    }
 
 ?>
