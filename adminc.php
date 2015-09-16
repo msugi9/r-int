5791046c72401080d94a2ba7343a22e19d2b8518
nikotaman07 <nikotaman07@ze.gs>
2015-09-16 15:19:50 +0900

    Add

--- adminc.php
+++ adminc.php
@@ -6,5 +6,5 @@ $name = ($_POST["name"]);
 $address = ($_POST["address"]);
 $tel = ($_POST["tel"]);
 $ski = ($_POST["ski"]);
-  echo "登録を受け付けました。\n会社名：$company\n責任者名:$name\n住所:$address\n電話番号:$tel\nスキー場:$ski"
+  echo "登録を受け付けました。\n会社名：$company\n責任者名:$name\n住所:$address\n電話番号:$tel\nスキー場:$ski";
 ?>
--- adminp.php
+++ adminp.php
@@ -2,6 +2,6 @@
 $id = ($_POST["id"]);
 $pass = ($_POST["password"]);
 $name = ($_POST["name"]);
-$mail = ($_POST["email"])
-  echo "登録を受け付けました。\n名前：$name\nメールアドレス:$email";
+$mail = ($_POST["email"]);
+echo"登録を受け付けました。\n名前：$name\nメールアドレス:$email";
 ?>
