16259a169d18689ad44073428f04ed614e2fa4a2
nikotaman05 <nikotaman05@ze.gs>
2015-09-16 19:09:23 +0900

    test

--- personalinform/personalinfo.php
+++ personalinform/personalinfo.php
@@ -1,10 +1,12 @@
 <?php
+    echo "登録完了しました。\n"    
 $database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
 $url = parse_url($database_url);
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 try{
     //データベース接続
     $pdo = new PDO($dsn, $url['user'], $url['pass']);
+    echo "test"
     $loginid=$_REQUEST[login_ID];
     $pass=$_REQUEST[login_pass];
     $height=$_REQUEST[height];
@@ -14,7 +16,7 @@ try{
     $wear=$_REQUESRT[wear];
     $board=$_REQUEST[board];
     $email=$_REQUEST[email];
-    echo "登録完了しました。"    
+
     $insert_sql = "insert into personal ($loginid,'$pass',$height,$weight,$shoe,$accessory,$wear,$board,'$email')";
 
     $sql = $pdo->exec($insert_sql); 
