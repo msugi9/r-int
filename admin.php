88281dee951206e62f8db95c3d7a9d5147978da5
nikotaman07 <nikotaman07@ze.gs>
2015-09-16 14:22:50 +0900

    Add

---
+++ admin.php
@@ -0,0 +1,11 @@
+<?php
+$company= ($_POST["company"]);
+$id = ($_POST["id"]);
+$pass = ($_POST["password"]);
+$name = ($_POST["name"]);
+$address = ($_POST["address"]);
+$tel = ($_POST["tel"]);
+$ski = ($_POST["ski"]);
+
+
+ ?>
--- confirm.php
+++ confirm.php
@@ -1,7 +1,6 @@
 <?php
 $id= ($_POST["id"]);
 $pass = ($_POST["password"]);
-var_dump($id);
-var_dump($pass);
+
 
  ?>
