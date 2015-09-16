69f8e47353fd22a6a1dda84f894070c29a362b32
nikotaman06 <nikotaman06@ze.gs>
2015-09-17 02:08:51 +0900

    herokuTest-0208

--- apply/apply.php
+++ apply/apply.php
@@ -29,21 +29,11 @@ $numOfMember = 3;
     <form action="./confirm.php" method  ="post">
       <!--for文的な？-->
       <table border="1" width="500" cellspacing="0" cellpadding="5" bordercolor="#333333">
-        echo '<tr>';
-          echo '<td>氏名</td><td>板ブーツ</td><td>ウェア</td><td>小物</td>';
-        echo '</tr>';
-        <?php
-        for($personId;$personId<$numOfPerson;$personId++){
-          echo '<tr>';
-          echo '<td>名前' .$personId .'</td>';
-          echo '<td><input type="checkbox" name="prsn'.$personId.'"></td>';
-          echo '<td><input type="checkbox" name="boad'.$personId.'"></td>';
-          echo '<td><input type="checkbox" name="wear'.$personId.'"></td>';
-          echo '<td><input type="checkbox" name="acce'.$personId.'"></td>';
-          echo '</tr>';
-        }?>
+        <tr>
+          <td>氏名</td><td>板ブーツ</td><td>ウェア</td><td>小物</td>
+        </tr>
         <?php foreach ($data as $personData) : ?>
-        $prsnId = prsn<?php echo $personData['id'];?>
+        <?php $prsnId = "prsn" .$personData['id'];?>
         <?php if($_REQUEST['$prsnId']){  ?>
           <tr style="background-color: #ffffff  ;">
             <td align="left"><?php echo $personData['name']; ?></td>
