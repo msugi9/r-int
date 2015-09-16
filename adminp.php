34130fc5bf92c01b945ffdb7b15a7487ce9a8ee7
nikotaman07 <nikotaman07@ze.gs>
2015-09-16 15:14:13 +0900

    Add

--- adminp.php
+++ adminp.php
@@ -3,5 +3,5 @@ $id = ($_POST["id"]);
 $pass = ($_POST["password"]);
 $name = ($_POST["name"]);
 $mail = ($_POST["email"])
-  echo "登録を受け付けました。\n名前：$name\nメールアドレス:$email"
+  echo "登録を受け付けました。\n名前：$name\nメールアドレス:$email";
 ?>
