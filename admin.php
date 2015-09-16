3b8fbd34110c0fa50738ada8eed4de1fc0630098
nikotaman07 <nikotaman07@ze.gs>
2015-09-16 14:55:07 +0900

    Add

--- admin.php
+++ admin.php
@@ -6,10 +6,5 @@ $name = ($_POST["name"]);
 $address = ($_POST["address"]);
 $tel = ($_POST["tel"]);
 $ski = ($_POST["ski"]);
-  echo "登録を受け付けました。\n　
-  会社名：$company
-  責任者名:$name
-  住所:$address
-  電話番号:$tel
-  スキー場:$ski"
+  echo "登録を受け付けました。\n会社名：$company\n責任者名:$name\n住所:$address\n電話番号:$tel\nスキー場:$ski"
 ?>
