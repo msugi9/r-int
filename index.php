014ad60d75f34753d6886408897359896b977097
drahi41 <drahi41@gmail.com>
2015-09-16 14:36:45 +0900

    sql test

--- index.php
+++ index.php
@@ -4,39 +4,42 @@
 
 <?php
 
-$conn = "host=ec2-54-204-30-115.compute-1.amazonaws.com dbname=d8seqgbs15lak9 user=jqczyyfqfondlh password=AVywYkXKpxTnzKtlbyr8wxIFQN";
-$link = pg_connect($conn);
-if (!$link) {
-    die('接続失敗です。'.pg_last_error());
-}
+// $conn = "host=ec2-54-204-30-115.compute-1.amazonaws.com dbname=d8seqgbs15lak9 user=jqczyyfqfondlh password=AVywYkXKpxTnzKtlbyr8wxIFQN";
+// $link = pg_connect($conn);
+// if (!$link) {
+//     die('接続失敗です。'.pg_last_error());
+// }
 
-print('接続に成功しました。<br>');
+// print('接続に成功しました。<br>');
 
 // PostgreSQLに対する処理
 $url = parse_url(getenv('DATABASE_URL'));
 
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 
-$pdo = new PDO($dsn, $url['user'], $url['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-$sql = "select * from ski_resort";
-echo "<pre>";
-var_dump($pdo->query($sql));
-exit;
-foreach($pdo->query($sql) as $row){
-  print(convert_enc($row['id']));
-  print(convert_enc($row['name']));
+try{
+  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+  $sql = "select * from ski_resort";
+  //$stmt = $pdo->query($sql);
+  foreach($pdo->query($sql) as $row){
+    print($row['id']);
+    print($row['name']);
+  }
+}catch(PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
 }
 
 // $result = pg_query($link, $sql);
 // var_dump($result);
 
 
-$close_flag = pg_close($link);
-if ($close_flag){
-    print('切断に成功しました。<br>');
-}
-
+// $close_flag = pg_close($link);
+// if ($close_flag){
+//     print('切断に成功しました。<br>');
+// }
+$pdo = null;
 
 ?>
 </body>
