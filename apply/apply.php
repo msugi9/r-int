4b7dbea5eab19c1d5c62b2a5056c51c516f1025a
nikotaman06 <nikotaman06@ze.gs>
2015-09-16 14:42:14 +0900

    herokuTest-1438

--- apply/apply.php
+++ apply/apply.php
@@ -3,34 +3,29 @@
 //$relatedPerson = ;
 $personId = 1;
 $numOfPerson = 5;
-echo "test";
 ?>
 <html>
-  <head><title>Test Template</title>
+  <head><title>メンバー決定</title>
     <body>
-      My Home Directory is <TMPL_VAR NAME=HOME>
+      参加者を選択してください<TMPL_VAR NAME=HOME>
       <p>
-        My Path is set to <TMPL_VAR NAME=PATH>
-        <form action="cgi-bin/abc.cgi" method="post">
-          <p>
-            名前：<input type="text" name="namae">
-          </p>
-          
-          <!--for文的な？-->
-          <table>
-            <?php
-            for($personId;$personId<$numOfPerson;$personId++){
-              echo "<tr>";
-              echo "<td>名前" .$personId ."</td>";
-              echo '<td><input type="checkbox" name="prsn".$personId . value="1" ></td>';
-              echo '</tr>';
-             }?>
-          </table>
-          <!--for文的な？-->
-          
-          
-          <input type="submit" value="メンバー確定">
-        </p>
-      </form>
-    </body>
-  </html>
+        <!--for文的な？-->
+        <center>
+        <table>
+          <?php
+          for($personId;$personId<$numOfPerson;$personId++){
+            echo "<tr>";
+            echo "<td>名前" .$personId ."</td>";
+            echo '<td><input type="checkbox" name="prsn".$personId . value="1" ></td>';
+            echo '</tr>';
+          }?>
+        </table>
+        </center>
+        <!--for文的な？-->
+        
+        
+        <input type="submit" value="メンバー確定">
+      </p>
+    </form>
+  </body>
+</html>
