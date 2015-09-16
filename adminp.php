324ef4f75ac301a4008f2209e5190936b550b9f4
nikotaman07 <nikotaman07@ze.gs>
2015-09-16 16:00:03 +0900

    Add

--- adminp.php
+++ adminp.php
@@ -1,7 +1,31 @@
 <?php
+$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
+//Postgresqlの接続に必要なデータの取得
+$url = parse_url($database_url);
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
+
+$pdo = null;
 $id = ($_POST["id"]);
 $pass = ($_POST["password"]);
 $name = ($_POST["name"]);
 $mail = ($_POST["email"]);
+
+
+try{
+  //データベースに接続
+  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  //sql文
+  $sql = "insert into personal (id,login_password,name,email)" . ($id,$pass,$name,$mail);
+
+  $result = $pdo->query($sql);
+  $data = $result->fetchAll();
+
+}catch(PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+}
+
+
 echo"登録を受け付けました。\n名前：$name\nメールアドレス:$mail";
 ?>
