86edf414394e9e7fd0f41a7c4b5730c56f736967
nikotaman06 <nikotaman06@ze.gs>
2015-09-16 15:10:21 +0900

    herokuTest-1510

--- apply/apply.php
+++ apply/apply.php
@@ -2,21 +2,27 @@
 //$userId = ;
 //$relatedPerson = ;
 $personId = 1;
-$numOfPerson = 5;
+$numOfMember = 3;
 ?>
 <html>
-  <head><title>メンバー決定</title>
+  <head><title>レンタル品確認</title>
     <body>
-      参加者を選択してください<TMPL_VAR NAME=HOME>
+      レンタル品とその数を確認してください<TMPL_VAR NAME=HOME>
       <p>
         <!--for文的な？-->
         <center>
-        <table>
+        <table border="1" width="500" cellspacing="0" cellpadding="5" bordercolor="#333333">
+          echo "<tr>";
+            echo '<td>氏名</td><td>板ブーツ</td><td>ウェア</td><td>小物</td>';
+          echo '</tr>';
           <?php
           for($personId;$personId<$numOfPerson;$personId++){
             echo "<tr>";
             echo "<td>名前" .$personId ."</td>";
             echo '<td><input type="checkbox" name="prsn'.$personId"'></td>';
+            echo '<td><input type="checkbox" name="boad'.$personId"'></td>';
+                echo '<td><input type="checkbox" name="wear'.$personId"'></td>';
+            echo '<td><input type="checkbox" name="acce'.$personId"'></td>';
             echo '</tr>';
           }?>
         </table>
