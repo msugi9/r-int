f4f750947832659c61f9bb7f440da47ebd215d9d
drahi41 <drahi41@gmail.com>
2015-09-16 16:42:45 +0900

    add database_url

--- db-sample.php
+++ db-sample.php
@@ -1,7 +1,8 @@
 
 <?php
+$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
 //Postgresqlの接続に必要なデータの取得
-$url = parse_url(getenv('DATABASE_URL'));
+$url = parse_url($database_url);
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 try{
   //データベースに接続
