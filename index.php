d64459d57027b991c46966ddc3aa9216a7fc6548
drahi41 <drahi41@gmail.com>
2015-09-16 14:46:41 +0900

    sql test

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
-<html>
-<head><title>PHP TEST</title></head>
-<body>
 
 <?php
 
-// $conn = "host=ec2-54-204-30-115.compute-1.amazonaws.com dbname=d8seqgbs15lak9 user=jqczyyfqfondlh password=AVywYkXKpxTnzKtlbyr8wxIFQN";
-// $link = pg_connect($conn);
-// if (!$link) {
-//     die('接続失敗です。'.pg_last_error());
-// }
-
-// print('接続に成功しました。<br>');
-
-// PostgreSQLに対する処理
+//Postgresqlの接続に必要なデータの取得
 $url = parse_url(getenv('DATABASE_URL'));
-
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 
 try{
+  //データベースに接続
   $pdo = new PDO($dsn, $url['user'], $url['pass']);
-  var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+  //var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+  //sql文
   $sql = "select * from ski_resort";
-  //$stmt = $pdo->query($sql);
+  $data = $pdo->query($sql);
   foreach($pdo->query($sql) as $row){
     print($row['id']);
     print($row['name']);
@@ -31,16 +21,31 @@ try{
   die();
 }
 
-// $result = pg_query($link, $sql);
-// var_dump($result);
+$prefecture = array(
+                '1'=>'北海道', '2'=>'青森県', '3'=>'岩手県', '4'=>'宮城県', '5'=>'秋田県',
+                '6'=>'山形県', '7'=>'福島県', '8'=>'茨城県', '9'=>'栃木県', '10'=>'群馬県',
+                '11'=>'埼玉県', '12'=>'千葉県', '13'=>'東京都', '14'=>'神奈川県', '15'=>'新潟県',
+                '16'=>'富山県', '17'=>'石川県', '18'=>'福井県', '19'=>'山梨県', '20'=>'長野県',
+                '21'=>'岐阜県', '22'=>'静岡県', '23'=>'愛知県', '24'=>'三重県', '25'=>'滋賀県',
+                '26'=>'京都府', '27'=>'大阪府', '28'=>'兵庫県', '29'=>'奈良県', '30'=>'和歌山県',
+                '31'=>'鳥取県', '32'=>'島根県', '33'=>'岡山県', '34'=>'広島県', '35'=>'山口県',
+                '36'=>'徳島県', '37'=>'香川県', '38'=>'愛媛県', '39'=>'高知県', '40'=>'福岡県',
+                '41'=>'佐賀県', '42'=>'長崎県', '43'=>'熊本県', '44'=>'大分県', '45'=>'宮崎県',
+                '46'=>'鹿児島県', '47'=>'沖縄県'
+);
 
-
-// $close_flag = pg_close($link);
-// if ($close_flag){
-//     print('切断に成功しました。<br>');
-// }
 $pdo = null;
 
+?>
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+<?php
+foreach($data as $data){
+  print_r($row['id']);
+  print_r($row['name']);
+  print_r($prefecture[$row['pref_code']]);
+}
 ?>
 </body>
 </html>
