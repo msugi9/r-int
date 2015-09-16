9eb4d386727c5e61b3be9c42eaeb26205a2d9d4f
nikotaman05 <nikotaman05@ze.gs>
2015-09-16 19:37:39 +0900

    test

--- personalinform/personalinfo.php
+++ personalinform/personalinfo.php
@@ -1,21 +1,23 @@
 <?php
-echo "Hello";
-//$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
-//$url = parse_url($database_url);
-//$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-//try{
+
+$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
+$url = parse_url($database_url);
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+try{
     //データベース接続
-    //$pdo = new PDO($dsn, $url['user'], $url['pass']);
-  //  echo "test"
-    /*$loginid=$_REQUEST[login_ID];
-    //$pass=$_REQUEST[login_pass];
-    //$height=$_REQUEST[height];
-    //$weight=$_REQUEST[weight];
-    //$shoe=$_REQUEST[shoe_size];
-    //$accessory=$_REQUEST[accessory];
-    //$wear=$_REQUESRT[wear];
-    //$board=$_REQUEST[board];
-    //$email=$_REQUEST[email];
+    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  
+    $loginid=$_REQUEST[login_ID];
+    $pass=$_REQUEST[login_pass];
+    $height=$_REQUEST[height];
+    $weight=$_REQUEST[weight];
+    $shoe=$_REQUEST[shoe_size];
+    $accessory=$_REQUEST[accessory];
+    $wear=$_REQUESRT[wear];
+    $board=$_REQUEST[board];
+    $email=$_REQUEST[email];
+
+    echo "hello";
 
     //$insert_sql = "insert into personal ($loginid,'$pass',$height,$weight,$shoe,$accessory,$wear,$board,'$email')";
 
@@ -23,16 +25,16 @@ echo "Hello";
 
 
     
-    //echo "身長...'$height'\n"
-    //echo "体重...'$weight'\n"
-    //echo "靴のサイズ...'$shoe'\n"
-    //echo "アクセサリー(キャップ、グローブ、ゴーグル)...'$accessory'\n"
-    //echo "ウェア...'$wear'\n"
-    //echo "ボード...'$board'\n"
+    //echo "身長...'$height'\n";
+    //echo "体重...'$weight'\n";
+    //echo "靴のサイズ...'$shoe'\n";
+    //echo "アクセサリー(キャップ、グローブ、ゴーグル)...'$accessory'\n";
+    //echo "ウェア...'$wear'\n";
+    //echo "ボード...'$board'\n";
     
-//}catch(PDOException $e){
-  //print('Error:'.$e->getMessage());
-  //die();
-    //}
+}catch(PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+    }
 
 ?>
