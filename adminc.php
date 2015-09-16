852078a8dcbd678602904e74ede9fbcafe1dfeee
nikotaman07 <nikotaman07@ze.gs>
2015-09-16 21:28:10 +0900

    Add

--- adminc.php
+++ adminc.php
@@ -1,10 +1,34 @@
 <?php
-$company= ($_POST["company"]);
+$pdo = null;
+$name= ($_POST["company"]);
 $id = ($_POST["id"]);
 $pass = ($_POST["password"]);
-$name = ($_POST["name"]);
+$charge = ($_POST["name"]);
 $address = ($_POST["address"]);
 $tel = ($_POST["tel"]);
 $ski = ($_POST["ski"]);
-  echo "登録を受け付けました。\n会社名：$company\n責任者名:$name\n住所:$address\n電話番号:$tel\nスキー場:$ski";
+try{
+
+  //データベースに接続
+  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  //sql文
+  $sql = "insert into company (login_id,login_password,name,charge,address,tel,ski_resort_id) values ( '$id','$pass','$name','$charge','$address','$tel','$ski')";
+
+  $result = $pdo->exec($sql);
+  //$data = $result->fetchAll();
+ $pdo = null;
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+  $sql2 = "select name from company where login_id = '$id'";
+  $result1 = $pdo->query($sql2);
+  $name1 = $result1->fetchAll();
+
+echo $name1;
+
+}catch(PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+}
+echo"登録を受け付けました。";
+?>
+
 ?>
