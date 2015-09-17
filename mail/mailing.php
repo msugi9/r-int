74c77dcf80a7dbf4964a40a7b5ac06c61d7694bf
nikotaman05 <nikotaman05@ze.gs>
2015-09-17 15:25:31 +0900

    test commit

--- mail/mailing.php
+++ mail/mailing.php
@@ -1,10 +1,10 @@
 <?php
-$mail1=$_REQUEST['mail_1'];
-$mail2=$_REQUEST['mail_2'];
-$mail3=$_REQUEST['mail_3'];
-$cmail1=$_REQUEST['conf_mail_1'];
-$cmail2=$_REQUEST['conf_mail_2'];
-$cmail3=$_REQUEST['conf_mail_3'];
+$mail=htmlspecialchars($_REQUEST['mail_1']);
+//$mail2=$_REQUEST['mail_2'];
+//$mail3=$_REQUEST['mail_3'];
+$cmail=htmlspecialchars($_REQUEST['conf_mail_1']);
+//$cmail2=$_REQUEST['conf_mail_2'];
+//$cmail3=$_REQUEST['conf_mail_3'];
 
 $subject="subjecttest";
 
@@ -14,19 +14,19 @@ if(strcmp($mail1,$cmail1) != 0){
     $judge=1;
 }
 
-if(strcmp($mail2,$cmail2) != 0){
+/*if(strcmp($mail2,$cmail2) != 0){
     $judge=1;
 }
 
 if(strcmp($mail3,$cmail3) != 0){
     $judge=1;
 }
-
+*/
 if($judge == 1){
     echo "もう一度入力して下さい<br>";
     print '<a href="https://blooming-dawn-8215.herokuapp.com/mail/mailform.html">招待ページに戻る</a>';
 }else{
-    
+    echo "$mail <br />";
     echo "招待メールを送信しました<br />";
 
     print '<a href="TOPページ">TOPページに戻る</a>';
