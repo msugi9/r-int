93aeb98e28e40c81e580798c24d7b8ab39e04d16
nikotaman05 <nikotaman05@ze.gs>
2015-09-18 05:24:35 +0900

    test commit

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <html>
-<head>
-  <title>TOP</title>
-</head>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      <title>TOP</title>
+      
+      <!-- Bootstrap -->
+      <link href="/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+      
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+
 <body>
-<a href="/register/loginp.php">個人ログイン</a>
+  <div class="panel panel-primary">
+    
+    <div class="panel-heading">
+      <p style="text-align: center">楽々スノボ</p>
+    </div>
+    <div class="list-group">
+      <a class="list-group-item" href="/register/loginp.php">個人ログイン</a>
+      <a class="list-group-item" href="/register/adminp.php">個人新規ログイン</a>
+      <a class="list-group-item" href="/register/loginc.php">企業ログイン</a>
+      <a class="list-group-item" href="/register/adminc1.php">企業新規ログイン</a>
+    </div>
+  </div>
+   
+<!--<a href="/register/loginp.php">個人ログイン</a>
 <a href="/register/adminp.php">個人新規ログイン</a>
 <a href="/register/loginc.php">企業ログイン</a>
-<a href="/register/adminc1.php">企業新規ログイン</a>
+<a href="/register/adminc1.php">企業新規ログイン</a>-->
 </body>
 </html>
