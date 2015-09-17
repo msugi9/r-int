6688b874de60222e6a05a05abfb92f7c86f2bf51
nikotaman08 <nikotaman08@ze.gs>
2015-09-18 05:56:54 +0900

    change

---
+++ company/itemConfirm.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
+$url = parse_url($database_url);
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
+$wear = $_REQUEST['wear'];
+$wearPrice = $_REQUEST['wear_price'];
+$board = $_REQUEST['board'];
+$boardPrice = $_REQUEST['board_price'];
+$accessory = $_REQUEST['accessory'];
+$accessoryPrice = $_REQUEST['accessory_price'];
+
+
+try{
+    //データベース接続
+    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+    $accessorySql = "insert into item (conpany_id,item_fee,available_flg,item_code)values(" . $_SESSION["company_id"] . ", '$accessory_price', '$accessory', 1)";
+    $sql = $pdo->exec($accessorySql);
+    $sql = null;
+
+    $boardSql = "insert into item (conpany_id,item_fee,available_flg,item_code)values(" . $_SESSION["company_id"] . ", '$board_price', '$board', 2)";
+    $sql = $pdo->exec($boardSql);
+    $sql = null;
+
+    $wearSql = "insert into item (conpany_id,item_fee,available_flg,item_code)values(" . $_SESSION["company_id"] . ", '$wear_price', '$wear', 3)";
+    $sql = $pdo->exec($wearSql);
+
+}catch(PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+    }
+
+?>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+登録しました
+<br>
+<a href="/top/company_top.php">TOPページに戻る</a>
+</body>
+</html>
+
