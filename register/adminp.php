e4f7b668940d24d421ed769545f8e9bae742438f
nikotaman08 <nikotaman08@ze.gs>
2015-09-17 17:14:22 +0900

    test session

--- register/adminp.php
+++ register/adminp.php
@@ -39,14 +39,14 @@ $pdo = new PDO($dsn, $url['user'], $url['pass']);
   die();
 }
 session_start();
-$_SESSION["personal_id"] = $personal_id["id"]["id"];
+$_SESSION["personal_id"] = $personal_id["id"];
 echo "<pre>";
 var_dump($personal_id);
-var_dump($personal_id[0]["id"]);
+var_dump($personal_id["id"]);
 
 print_r("登録を受け付けました。");
 print_r("２秒後にリダイレクトします。");
 var_dump($_SESSION["personal_id"]);
 
-// header( "refresh:2;url=/personalinform/personalinfo.html" );
+header( "refresh:2;url=/personalinform/personalinfo.html" );
 ?>
