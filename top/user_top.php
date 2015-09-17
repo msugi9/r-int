852c9a9807b07cce0fb8e30bfd0fb49195f6de9b
nikotaman05 <nikotaman05@ze.gs>
2015-09-18 02:14:42 +0900

    test commit

--- top/user_top.php
+++ top/user_top.php
@@ -20,23 +20,52 @@ try{
 }
 ?>
 <html>
-<head>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+    
+    <!-- Bootstrap -->
+    <link href="../bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+  </head>
+
+<!--<head>
   <title>User Top</title>
-</head>
-<body>
-  <h1 style="text-align: center">
-    <?php echo $name[0]["name"]; ?>さんのトップページ
-  </h1>
-  <table border="1" align="center">
-    <tr>
-      <th><a href="/apply/decide_person.php">レンタル申し込み</a></th>
-      <th><a href="/mail/mailform.php">招待</a></th>
-    </tr>
-    <tr>
-      <th><a href="/past/index.php">過去データ</a></th>
-      <th><a href="/personalinform/personal_form.php">個人データ変更</a></th>
-    </tr>
-  </table>
+  </head> -->
+  
+  <body>
+    <div class="page-header">
+      <h1><span class="label label-warning">楽々スノボ</span></h1>
+    </div>
+
+      <h1 style="text-align: center">
+	<span class="label laber-info">
+	  <?php echo $name[0]["name"]; ?>さんのトップページ
+	</span>
+      </h1>
+
+      <div class="container">
+	<table class="table" border="1" align="center">
+	  <tr>
+	    <th class="success"><a href="/apply/decide_person.php">レンタル申し込み</a></th>
+	    <th class="warning"><a href="/mail/mailform.php">招待</a></th>
+	  </tr>
+	  <tr>
+	    <th class="danger"><a href="/past/index.php">過去データ</a></th>
+	    <th class="info"><a href="/personalinform/personal_form.php">個人データ変更</a></th>
+	  </tr>
+	</table>
+      </div>
   <div style="margin-top: 30px;text-align: center">
     <a href="/top/logout.php">ログアウト</a>
   </div>
