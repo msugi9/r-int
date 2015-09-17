3ccda00a1bdce0bce6c9cec60da5d680f9d65bf5
nikotaman07 <nikotaman07@ze.gs>
2015-09-18 02:52:39 +0900

    change

--- past/index.php
+++ past/index.php
@@ -34,10 +34,11 @@ try{
 
   <?php
   $x = $data[0][4];
-  foreach($data as $tmp){
+  var_dump($x);
+  /*foreach($data as $tmp){
     if($x == $tmp[4])
     $child = $tmp[7];
-    echo $child;
+    echo $child;*/
   }
   else(echo $x);
   ?>
