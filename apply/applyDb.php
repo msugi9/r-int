bbe74a4399e1d3122e4858ad088eec10cc0c49b3
nikotaman06 <nikotaman06@ze.gs>
2015-09-17 23:26:34 +0900

    herokuTest-2326

--- apply/applyDb.php
+++ apply/applyDb.php
@@ -4,32 +4,39 @@ $database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204
 $url = parse_url($database_url);
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 
-
 $pdo = null;
 
 session_start();
 $personalId = $_SESSION["personal_id"];
-$companyId = ($_POST["password"]);
-$skiResortId = ($_POST["charge"]);
-$playDate = ($_POST["mail"]);
+$companyId = $_SESSION["company_id"];
+$skiResortId = $_SESSION["ski_resort_id"];
+$playDate = $_SESSION["play_date"];
+
 $userId = ($_POST["address"]);
 $itemId = ($_POST["tel"]);
 $ski = ($_POST["ski"]);
+
 try{
   
   //データベースに接続
   $pdo = new PDO($dsn, $url['user'], $url['pass']);
   //sql文
-  $apsql = "insert into apply (name,login_id,login_password,mail_address,charge,address,tel,ski_resort_id) values ( '$name','$id','$pass','$mail','$charge','$address',$tel,$ski)";
-  $result = $pdo->exec($sql);
+  $ssql = "select * from ski_resort";
+  $sresult = $pdo->query($ssql);
+  $persdata = $sresult->fetchAll();
   
-  foreach($data as $tmp)
+  $apsql = "insert into apply (personal_id, company_id, ski_resort_id, play_date) values ('$personalId','$companyId','$skiResortId','$playDate')";
+  $apresult = $pdo->exec($apsql);
+  if (!$apresult) {
+    exit('データを登録できませんでした。');
+  }
+  /*
+  foreach($data as $tmp)//applu_itemのDBへの入力。無理だすまん。
   if(1){
     $apisql = "insert into apply_item values ( '$_POST[""],'$_POST[""]','$_POST[""]')";
     $result = $pdo->exec($sql);
   }
-  
-  
+  */
 }catch(PDOException $e){
   print('Error:'.$e->getMessage());
   die();
