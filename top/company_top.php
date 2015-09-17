c222940f7ac79d127e74c9acc30fa2740c7f9fa2
nikotaman08 <nikotaman08@ze.gs>
2015-09-18 04:31:08 +0900

    change

--- top/company_top.php
+++ top/company_top.php
@@ -1,13 +1,32 @@
 <?php
 session_start();
+$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
+//Postgresqlの接続に必要なデータの取得
+$url = parse_url($database_url);
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
+try{
+  //データベースに接続
+  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  //sql文
+  $sql = "select name from company where id = " . $_SESSION["company_id"];
+
+  $result = $pdo->query($sql);
+  $name = $result->fetchAll();
+
+}catch(PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+}
 ?>
+
 <html>
 <head>
   <title>User Top</title>
 </head>
 <body>
   <h1 style="text-align: center">
-     トップページ
+     <?php echo $name[0]["name"]; ?>様のトップページ
   </h1>
   <div style="center">
     <a href="../company/order.php">注文一覧</a>
