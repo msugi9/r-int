b0a56d29b90a4c04877652ceae2c361462251e2f
nikotaman08 <nikotaman08@ze.gs>
2015-09-17 19:53:45 +0900

    change update sql

--- top/user_top.php
+++ top/user_top.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+$database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
+//Postgresqlの接続に必要なデータの取得
+$url = parse_url($database_url);
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
 try{
   //データベースに接続
   $pdo = new PDO($dsn, $url['user'], $url['pass']);
