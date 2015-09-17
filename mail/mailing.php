2841b773a9c7ed6ed88787e79342d9618aa29bb9
nikotaman05 <nikotaman05@ze.gs>
2015-09-17 23:53:45 +0900

    test commit

--- mail/mailing.php
+++ mail/mailing.php
@@ -18,18 +18,34 @@ try{
 	$judge=1;
     }
     
-    if($judge == 1){
-	echo "もう一度入力して下さい<br>";
-	print '<ahref="/mailform.php">招待ページに戻る</a>';
-    }else{
-	echo "$mail <br />";
-	echo "招待メールを送信しました<br />";
-	print '<a href="/advertising.php">招待メールを見る</a><br />';
-	print '<a href="../top/user_top.php">TOPページに戻る</a><br />';
-    }
 }catch(PDOException $e){
     print('Error:'.$e->getMessage());
     die();
 }
 
 ?>
+
+<html
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+    
+    <!-- Bootstrap -->
+    <link href="../bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+  </head>
+  <body bgcolor="#fffacd  " TEXT="#333333  " LINK="#3333cc  ">
+    <h2 class="text-primary">
+      <?php if($judge==1){echo "もう一度入力して下さい<br>";print '<ahref="/mailform.php">招待ページに戻る</a>';}else{echo "$mail <br />";echo "招待メールを送信しました<br />";print '<a href="/advertising.php">招待メールを見る</a><br />';print '<a href="../top/user_top.php">TOPページに戻る</a><br />';}?>
+    </h2>
+</html>
