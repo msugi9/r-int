6d067089cb72a11e77bb96604a2f20e28a31afc5
nikotaman05 <nikotaman05@ze.gs>
2015-09-18 04:32:20 +0900

    test commit

--- register/adminc1.php
+++ register/adminc1.php
@@ -20,33 +20,83 @@ try{
 }
  ?>
 <HTML>
-<FORM action="adminc.php" method="post">
-  <title>楽天　スノーボードレンタルサイト　登録用</title>
-  <b>楽天　スノーボードレンタルサイト　登録用(会社)</b>
-  <br>
-会社名<input type = "text" name ="name";>
-<br>
-ID<input type = "text" name ="id";>
-<br>
-password<input type = "text" name ="password";>
-<br>
-責任者名<input type = "text" name ="charge";>
-<br>
-メール<input type = "text" name ="mail";>
-<br>
-住所<input type = "text" name ="address";>
-<br>
-TEL<input type = "text" name ="tel";>
-<br>
-スキー場<select name="ski">
-  <?php
-  foreach ($ski_resort as $value) {
-    $option = '<option value="' . $value['id'] . '">' . trim($value['name']) . '</option>';
-    echo $option;
-  }
- ?>
-</select>
-<br>
-<input type ="submit" name ="to-roku" value="登録";>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      <title>楽々スノボ登録ページ</title>
+      
+      <!-- Bootstrap -->
+      <link href="/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+      
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+    <body bgcolor="#fffacd  " TEXT="#333333  " LINK="#3333cc  ">
+
+<FORM action="adminc.php" method="post" style="mergin: 0px 25%">
+	<div class="page-header">
+	  <h1><span class="label label-warning label-inline">楽々スノボ</span></h1>
+	  <h1 class="text-primary" style="text-align: center">
+	    <span class="text-info text-inline" style="text-align: center">
+	      楽々スノボ　新規登録(企業用)
+	    </span>
+	  </h1>
+	</div>
+	<br>
+	<div class="input-group">
+	  <span class="input-group-addon"> company name</span>
+	  <input type="text" name="name" class="form-control" placeholder="who">
+	</div>
+	<br>
+	<div class="input-group">
+	  <span class="input-group-addon"> login id</span>
+	  <input type="text" name="id" class="form-control" placeholder="***">
+	</div>
+	<br>
+	<div class="input-group">
+	  <span class="input-group-addon"> login password</span>
+	  <input type="text" name="password" class="form-control" placeholder="***">
+	</div>
+	<br>
+	<div class="input-group">
+	  <span class="input-group-addon"> responsible person's name</span>
+	  <input type="text" name="charge" class="form-control" placeholder="who">
+	</div>
+	<br>
+	<div class="input-group">
+	  <span class="input-group-addon"> email</span>
+	  <input type="text" name="email" class="form-control" placeholder="test@test.com">
+	</div>
+	<br>
+	<div class="input-group">
+	  <span class="input-group-addon"> address</span>
+	  <input type="text" name="address" class="form-control" placeholder="東京都足立区2-**-1">
+	</div>
+	<br>
+	<div class="input-group">
+	  <span class="input-group-addon"> telphone number</span>
+	  <input type="text" name="tel" class="form-control" placeholder="00001113333">
+	</div>
+	<br>
+	place od ski<select name="ski">
+	  <?php
+	  foreach ($ski_resort as $value) {
+	      $option = '<option value="' . $value['id'] . '">' . trim($value['name']) . '</option>';
+	      echo $option;
+	  }
+	  ?>
+	</select>
+	<br>
+	<p>
+	  <button type="submit" name="to-roku" value="登録" class="btn btn-primary">regist</button>
+	</p>
+
 </FORM>
 </HTML>
--- register/loginc.php
+++ register/loginc.php
@@ -1,13 +1,60 @@
+<?php
+session_start();
+if($_SESSION["personal_id"]){
+  header("Location:/top/user_top.php");
+  exit;
+}
+
+$parent_id = ($_GET["parent"]);
+?>
 <HTML>
-<FORM action="confirmc.php" method="post">
-  <title>楽天　スノーボードレンタルサイト　ログイン</title>
-  <b>楽天　スノーボードレンタルサイト　ログイン（会社）</b>
+  <html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>楽々スノボログインページ</title>
+
+  <!-- Bootstrap -->
+  <link href="/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body bgcolor="#fffacd  " TEXT="#333333  " LINK="#3333cc  ">
+  <FORM action="confirmp.php" method="post" style="margin: 0px 25%">
+  <!-- タイトル -->
+  <div class="page-header">
+    <h1><span class="label label-warning label-inline">楽々スノボ</span></h1>
+    <h1 class="text-primary" style="text-align: center">
+	<span class="text-info text-inline" style="text-align: center">
+	  楽々スノボ　ログイン(企業用)
+	</span>
+      </h1>
+  </div>
+
   <br>
-ID<input type = "text" name ="id";>
-<br>
-password<input type = "text" name ="password";>
+  <div class="input-group">
+	<span class="input-group-addon">login ID</span>
+	<input type="text" name="id" class="form-control" placeholder="hogehoge">
+  </div>
+  <br>
+  <div class="input-group">
+	<span class="input-group-addon">login password</span>
+	<input type="text" name="password" class="form-control" placeholder="hogehoge">
+  </div>
 
 <br>
-<input type ="submit" name ="login" value="login";>
+<input type="hidden" name="parent_id" value=<?php echo $parent_id; ?> />
+      <p>
+	<button type="submit" name="login" value="login" class="btn btn-primary">login</button>
+      </p>
+
 </FORM>
 </HTML>
+
