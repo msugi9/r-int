c4eb096687f14bab2ab7d625c0e38a4d88e346de
nikotaman08 <nikotaman08@ze.gs>
2015-09-17 18:16:13 +0900

    add session

--- personalinform/personal_form.php
+++ personalinform/personal_form.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-var_dump($_SESSION["personal_id"])
+//var_dump($_SESSION["personal_id"])
 ?>
 
 <html lang = "ja">
--- personalinform/personalinfo.php
+++ personalinform/personalinfo.php
@@ -3,10 +3,13 @@
 $database_url = "postgres://jqczyyfqfondlh:AVywYkXKpxTnzKtlbyr8wxIFQN@ec2-54-204-30-115.compute-1.amazonaws.com:5432/d8seqgbs15lak9";
 $url = parse_url($database_url);
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
+session_start();
+
 try{
     //データベース接続
     $pdo = new PDO($dsn, $url['user'], $url['pass']);
-  
+
     $loginid=htmlspecialchars($_REQUEST['login_ID']);
     $pass=htmlspecialchars($_REQUEST['login_pass']);
     $height=htmlspecialchars($_REQUEST['height']);
@@ -17,12 +20,12 @@ try{
     $board=htmlspecialchars($_REQUEST['board']);
     $email=htmlspecialchars($_REQUEST['email']);
 
-    $insert_sql = "update personal set height=$height,weight=$weight,shoe_size=$shoe,accessory=$accessory,wear=$wear,board=$board where id=";
+    $insert_sql = "update personal set height=$height,weight=$weight,shoe_size=$shoe,accessory=$accessory,wear=$wear,board=$board where id= " . $_SESSION["personal_id"];
 
-    $sql = $pdo->exec($insert_sql); 
+    $sql = $pdo->exec($insert_sql);
 
 
-    
+
     echo "身長...'$height'\n";
     echo "体重...'$weight'\n";
     echo "靴のサイズ...'$shoe'\n";
@@ -32,7 +35,8 @@ try{
 
     echo "登録しました\n";
 
-    print '<a href="TOPページ">TOPページに戻る</a>'
+    print '<a href="TOPページ">TOPページに戻る</a>';
+
 }catch(PDOException $e){
   print('Error:'.$e->getMessage());
   die();
