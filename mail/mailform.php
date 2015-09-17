008c02adb39efd4e1a72f4004d7c7a41fc63d86f
nikotaman05 <nikotaman05@ze.gs>
2015-09-18 03:28:02 +0900

    test commit

--- mail/mailform.php
+++ mail/mailform.php
@@ -2,16 +2,33 @@
 session_start();
 
 ?>
-<html lang = "ja">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title> 招待ページ</title>
-  </head>
 
-  <body bgcolor="#ffffff  " TEXT="#333333  " LINK="#3333cc  ">
-    <!-- タイトル -->
-    <h1 style = "font-size: 16px; font-weight: bold;">招待ページ</h1>
-    <!-- タイトル -->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>招待ページ</title>
+
+  <!-- Bootstrap -->
+  <link href="/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body bgcolor="#fffacd  " TEXT="#333333  " LINK="#3333cc  ">
+
+  <!-- タイトル -->
+  <div class="page-header">
+    <h1><span class="label label-warning">会員招待</span></h1>
+  </div>
+
+  <!-- タイトル -->
 
     <form action="./mailing.php" method="post">
       招待する人のURLを入力して下さい。
