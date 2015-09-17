1ef27fb3d529bc1b05de41bbdc38ad9c7e48b471
nikotaman05 <nikotaman05@ze.gs>
2015-09-18 01:41:11 +0900

    test commit

--- personalinform/personal_form.php
+++ personalinform/personal_form.php
@@ -23,12 +23,26 @@ echo "<pre>";
 }
 ?>
 
-<html lang = "ja">
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <title>個人情報入力ページ</title>
-</head>
-
+<html
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+    
+    <!-- Bootstrap -->
+    <link href="../bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    
 <body bgcolor="#fffacd  " TEXT="#333333  " LINK="#3333cc  ">
   <!-- タイトル -->
   <div class="page-header">
