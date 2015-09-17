0e5cb07b33522a2b3a567d9282a0377579d502fa
nikotaman05 <nikotaman05@ze.gs>
2015-09-18 03:36:28 +0900

    test commit

--- mail/mailform.php
+++ mail/mailform.php
@@ -25,44 +25,28 @@ session_start();
 
   <!-- タイトル -->
   <div class="page-header">
-    <h1><span class="label label-warning">会員招待</span></h1>
+    <h1><span class="label label-danger">会員招待</span></h1>
   </div>
 
   <!-- タイトル -->
 
     <form action="./mailing.php" method="post">
       招待する人のURLを入力して下さい。
-      <table width="60%" border="0" cellspacing="0" cellpadding="0" style="margin: 2px 0pt 0pt 0px;">
-	      <tr>
-	          <td bgcolor="#afafaf  ">
-	              <table width="100%" border=0 cellspacing=1 cellpadding=5 style="font-size: 12px;">
-	                  <tr>
-		              <td style="background-color: #eeeeee  ;">招待するメールアドレス</td>
-		              <td style="background-color: #ffffff  ;">
-		                  <input type="text" name="mail_1" size="30">
-		              </td>
-	                  </tr>
-	              </table>
-	          </td>
-	      </tr>
-          </table>
+      <div class="input-group">
+	<span class="input-group-addon">招待するメールアドレス</span>
+	<input type="text" name="mail_1" class="form-control" placeholder="test@test.com">
+      </div>
       確認のためもう一度入力して下さい。
-    <table width="60%" border="0" cellspacing="0" cellpadding="0" style="margin: 5px 0pt 0pt 0px;">
-            <tr>
-	        <td bgcolor="#afafaf  ">
-	            <table width="100%" border=0 cellspacing=1 cellpadding=5 style="font-size: 12px;">
-	                <tr>
-	                    <td style="background-color: #eeeeee  ;">確認用メールアドレス</td>
-	                    <td style="background-color: #ffffff  ;">
-		                <input type="text" name="conf_mail_1" size="30">
-	                    </td>
-	                </tr>
-	            </table>
-	        </td>
-            </tr>
-        </table>
-        <br> <input type="submit" name="send" value="送信" style="WIDHT: 50px;HEIGHT: 30px">
+      <div class="input-group">
+	<span class="input-group-addon">招待するメールアドレス(確認用)</span>
+	<input type="text" name="conf_mail_1" class="form-control" placeholder="test@test.com">
+      </div>
+
+      <p>
+	<button type="submit" name="send" value="送信" class="btn btn-primary">送信</button>
+	<button type="button" class="btn btn-default" onclick="location.href='/top/user_top.php'">TOPに戻る</button>
+      </p>
+
       </form>
-    <a href="https://blooming-dawn-8215.herokuapp.com/top/user_top.php">TOPに戻る</a>
   </body>
   </html>
