41b253fa38baf31334153fb126a189408bd2accf
nikotaman08 <nikotaman08@ze.gs>
2015-09-18 09:38:10 +0900

    $commit_message

--- company/itemConfirm.php
+++ company/itemConfirm.php
@@ -12,27 +12,24 @@ $boardPrice = $_REQUEST['board_price'];
 $accessory = $_REQUEST['accessory'];
 $accessoryPrice = $_REQUEST['accessory_price'];
 
-var_dump($wear);
-var_dump($board);
-var_dump($accessory);
 
 try{
     //データベース接続
     $pdo = new PDO($dsn, $url['user'], $url['pass']);
 
-  if($accessory==1){
+  if($accessory=="1"){
     $accessorySql = "insert into item (conpany_id,item_fee,available_flg,item_code)values(" . $_SESSION["company_id"] . ", '$accessoryPrice', true, 1)";
     $sql = $pdo->exec($accessorySql);
     $sql = null;
   }
 
-  if($board==1){
+  if($board=="1"){
     $boardSql = "insert into item (conpany_id,item_fee,available_flg,item_code)values(" . $_SESSION["company_id"] . ", true, '$board', 2)";
     $sql = $pdo->exec($boardSql);
     $sql = null;
   }
 
-  if($wear==1){
+  if($wear=="1"){
     $wearSql = "insert into item (conpany_id,item_fee,available_flg,item_code)values(" . $_SESSION["company_id"] . ", true, '$wear', 3)";
     $sql = $pdo->exec($wearSql);
   }
